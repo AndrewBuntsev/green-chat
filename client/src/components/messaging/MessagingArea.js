dfbde36e0d5f06d7a76dd8f7b0f51b9cb7b9fa3c
andreibuntsev <andrew.buntsev@gmail.com>
2020-03-12 08:19:02 +1100

    Added messages autoscrolling

--- client/src/components/messaging/MessagingArea.js
+++ client/src/components/messaging/MessagingArea.js
@@ -11,10 +11,13 @@ const styles = {
     messagesBox: {
         border: 'green 1px solid',
         borderRadius: '5px',
-        height: '60%'
+        height: '500px',
+        overflow: 'auto',
+        listStyle: 'none'
     },
     messageOut: {
-        border: 'red 1px solid',
+        //border: 'red 1px solid',
+        backgroundColor: '#DDDDDD',
         borderRadius: '5px',
         marginBottom: '5px',
         width: '70%',
@@ -33,11 +36,36 @@ const styles = {
         backgroundColor: '#9999FF'
     }
 };
+
+const listStyle = {
+    width: '60%',
+    height: '100px',
+    overflow: 'auto',
+    listStyle: 'none',
+    background: '#d4f3e6',
+    fontFamily: 'Garamond, serif',
+    fontSize: '1.2rem',
+    border: '1px #a59b15 solid',
+    padding: '0.5em',
+    boxShadow: '5px 5px #d9d9d9',
+    borderRadius: '3px'
+};
+
+const listItemStyle = {
+    textAlign: 'left',
+    borderBottom: '1px gray dashed',
+    marginBottom: '0.1em',
+    paddingBottom: '0.3em'
+};
+
+
 export default class MessagingArea extends Component {
 
     state = {
         newMessage: ''
     };
+
+    chatRef = React.createRef();
 
     onClickSendMessage = () => {
         this.props.sendMessage(this.state.newMessage);
@@ -46,15 +74,32 @@ export default class MessagingArea extends Component {
 
     render() {
         const messages = this.props.contact.messages
-            ? this.props.contact.messages.map((m, i) => <div style={styles.messageOut} key={i}>{m.msg}</div>)
+            ? this.props.contact.messages.map((m, i) => <li style={styles.messageOut} key={i}>{m.msg}</li>)
             : [];
 
         return <div style={styles.container} key={this.props.contact.contactId}>
-            <div style={styles.messagesBox}>
+            <ul style={styles.messagesBox} ref={this.chatRef}>
                 {messages}
-            </div>
+            </ul>
             <textarea style={styles.newMessageBox} value={this.state.newMessage} onChange={e => this.setState({ newMessage: e.target.value })}></textarea>
             <button style={styles.sendMessageButton} onClick={this.onClickSendMessage}>Send</button>
         </div>;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        if (this.props.contact.messages
+            && prevProps.contact.messages
+            && this.props.contact.messages.length > prevProps.contact.messages.length) {
+            const chatRef = this.chatRef.current;
+            return chatRef.scrollHeight - chatRef.scrollTop;
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (snapshot !== null) {
+            const chatRef = this.chatRef.current;
+            chatRef.scrollTop = chatRef.scrollHeight - snapshot;
+        }
     }
 }
